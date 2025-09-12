01a916337342808b371cf0adab56bfb85acb957e
JuanMejia <juan.mejia@gers.com>
2025-09-12 12:50:16 -0500

    boton eliminar

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,18 +1,31 @@
 import { get } from "../services/api";
 import { useState, useEffect } from "react";
+import DeleteButton from "./btonDel";
 
 export default function Table(){
+
+    const url_ = "/employees";
+    const url_destroy = "/employees/document";
     
     const [employees, setEmployees] = useState([]);
+
+    // Función para obtener empleados
     const fetchEmployees = async () => {
         try {
-            const response = await get("/employees");
+            const response = await get(url_);
             setEmployees(response.data);
         } catch (error){
             console.error("Error al obtener empleados", error);
         }
     };
-        useEffect(() => {
+
+    // Función para actualizar la tabla después de eliminar un empleado
+    const deleteSucces = (employeeDocument) => {
+        setEmployees(prevEmployee => prevEmployee.filter(employee => employee.document !== employeeDocument));
+    };
+
+    // Cargar empleados al montar el componente
+    useEffect(() => {
         fetchEmployees();
     }, []);
 
@@ -34,10 +47,16 @@ export default function Table(){
                 <td>{employee.document}</td>
                 <td>{employee.email}</td>
                 <td>{employee.role?.rol}</td>
+                <td>
+                    <DeleteButton
+                        url={url_destroy}
+                        document={employee.document}
+                        onDelete={deleteSucces}
+                    />
+                </td>
             </tr>
             ))}
         </tbody>
         </table>
-   );
-
+    );
 }
